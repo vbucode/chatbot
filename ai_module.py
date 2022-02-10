41820e32506c825935c0b3651c0f211aad2f8fcb
Bulat Valiakhmetov <61677060+Bulatv2@users.noreply.github.com>
2022-02-11 04:08:55 +0500

    Update ai_module.py

--- ai_module.py
+++ ai_module.py
@@ -17,29 +17,33 @@ def getdata(xarg):
     count = 0
     count2 = 0
     count3 = 0
+    flag = 0
     answ = ""
+    text = "try again.."
     nlist = []
     word = Words(xarg)
     w = word.load()
-    print(w)
     for i in w:
         try:
             count = bdata.index(i)
+            flag = 0
         except ValueError:
-            break
+            flag = 1
         nlist.append(count)
-        print(nlist)
-    for j in vdata:
-        count2 = 0
-        for i in nlist:
-            if j[i] == 0:
-                count2 += 1
-                break
-            else:
-                count2 += 1
-                if count2 == len(nlist):
-                    answ = slist[count3]
+    if flag != 1:
+        for j in vdata:
+            count2 = 0
+            for i in nlist:
+                if j[i] == 0:
+                    count2 += 1
+                    break
                 else:
-                    answ = "try again.."
+                    count2 += 1
+                    if count2 == len(nlist):
+                        answ = slist[count3]
+                    else:
+                        answ = text
         count3 += 1
+    else:
+        answ = text
     return answ
