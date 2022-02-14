a6d3f0004f1f1d7b340a0f873a88ae32ca9c667f
Bulat Valiakhmetov <61677060+Bulatv2@users.noreply.github.com>
2022-02-14 13:59:26 +0500

    Update app.py

--- app.py
+++ app.py
@@ -11,45 +11,46 @@ with open("data.txt", "r") as file:
                 left, right, *res = line.split(":")
                 llist.append(left)
                 rlist.append(right)
-                varstring = " ".join(llist)
 def getdata(xarg):
     answ = ""
     count = 0
     count2 = 0
-    nlist = []
+    searchlist = []
     ilist = []
     listvect = []
     listvect2 = []
     vect = vector.vector(llist)
     word = Words(xarg)
     w = word.load()
-    instbow = Words(varstring)
-    instb = instbow.load()
+    instb = vector.bow()
     for k in w:
         for i, x in enumerate(instb):
             if x == k:
-                nlist.append(i)
+                searchlist.append(i)
     for j in vect:
         count = 0
-        for i in nlist:
+        for i in searchlist:
             if j[i] != 0:
                 count += 1
         count2 += 1
         if count != 0:
             listvect.append(count)
-            listvect2.append(count2-1)
+            listvect.append(count2-1)
+            listvect2.append(listvect)
 
-    for i in listvect:
-        y = i/len(nlist)
-        ilist.append(y)
-
-    if len(ilist) == 1:
+    if len(listvect2) == 1:
         for i in listvect2:
-            answ = rlist[i]
+            answ = rlist[i[1]]
     else:
-        for i in ilist:
-            if i == max(ilist):
-                m = ilist.index(i)
-                v = listvect2[m]
-                answ = rlist[v]
+        for i in listvect2:
+            for j in i:
+                if j[0]:
+                    x = j[0]/len(searchlist)
+                    ilist.append(x)
+        
+        if i == max(ilist):
+            m = ilist.index(i)
+            v = listvect2[m]
+            answ = rlist[v]
+
     return answ
