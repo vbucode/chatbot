00f1030993434fcb4e0f010c94abf2209d23c7c7
Bulat Valiakhmetov <61677060+Bulatv2@users.noreply.github.com>
2022-02-15 01:59:33 +0500

    Update app.py

--- app.py
+++ app.py
@@ -11,14 +11,12 @@ with open("data.txt", "r") as file:
                 left, right, *res = line.split(":")
                 llist.append(left)
                 rlist.append(right)
+
 def getdata(xarg):
     answ = ""
-    count = 0
-    count2 = 0
     searchlist = []
     ilist = []
     listvect = []
-    listvect2 = []
     vect = vector.vector(llist, tf = 0)
     word = Words(xarg)
     w = word.load()
@@ -29,27 +27,17 @@ def getdata(xarg):
                 searchlist.append(i)
     if len(searchlist) != 0:
         for j in vect:
-            count = 0
             for i in searchlist:
                 if j[i] != 0:
-                    count += 1
-                    count2 += 1
-                    if count != 0:
-                        listvect.append(count)
-                        listvect.append(count2-1)
-                        listvect2.append(listvect)
-        if len(listvect2) == 1:
-            for i in listvect2:
+                    listvect.append([1,vect.index(j)])
+        if len(listvect) == 1:
+            for i in listvect:
                 answ = rlist[i[1]]
         else:
-            for i in listvect2:
-                for j in i:
-                    if j[0]:
-                        x = j[0]/len(searchlist)
-                        ilist.append(x)
-            if i == max(ilist):
-                m = ilist.index(i)
-                v = listvect2[m]
-                answ = rlist[v]
+            for i in listvect:
+                ilist.append(i[1])
+            for i in ilist:
+                if i == max(ilist):
+                    answ = rlist[i]
 
     return answ
