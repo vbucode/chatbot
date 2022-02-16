f85e36f0202d8520c01db999b4305236dcba18ea
Bulat Valiakhmetov <61677060+Bulatv2@users.noreply.github.com>
2022-02-17 02:21:16 +0500

    Add files via upload

--- gui.py
+++ gui.py
@@ -1,25 +1,38 @@
-# Chatbot
+# chatbot
 # author Bulat
 
 import app
+import logging
 
-reply = "try again with more information.."
+#reply = "Я вас не понимаю, пожалуйста, повторите"
+reply = "more information.."
 
-print("Welcome to: Chatbot")
+print("Welcome to: ChatBot")
 
 def main():
     global user
     user = input("\nYou: ")
+    logging.info(user)
     ai(user)
 def ai(data):
     if not data:
         main()
     elif data == "exit":
         exit()
+    elif len(data) > 40:
+        wrong()
     send = app.getdata(data)
-    if send != "":
-        print("\nChatbot: {}".format(send))
+    if send == "":
+        wrong()
     else:
-        print("\nChatbot: {}".format(reply))
+        print("\nChatbot: {}".format(send))
+        logging.info(send)
+
+def wrong():
+    print("\nChatbot: {}".format(reply))
+    logging.info(reply)
+
+logging.basicConfig( level=logging.DEBUG, filename = "mylog.log", filemode="a", format = "%(asctime)s - %(module)s - %(levelname)s - %(funcName)s: %(lineno)d - %(message)s", datefmt='%H:%M:%S')
+
 while  True:
     main()
