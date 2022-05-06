fde1c8884274bdbc1158424acf066ba5ff9884f4
vbucode <61677060+vbucode@users.noreply.github.com>
2022-05-06 20:34:46 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,30 +1,70 @@
-from app import App
 import logging
+from words import Words
+from wordvector import WordVector
+
+llist = []
+rlist = []
+dlist = []
 
 #reply = "Я вас не понимаю, пожалуйста, повторите"
+
 reply = "more information.."
 
 print("Welcome to: ChatBot")
 
+with open("data.txt", "r") as file:
+    for line in file:
+        if not line:
+            continue
+        else:
+            left, right, *res = line.split(":")
+            llist.append(left)
+            rlist.append(right.replace("\n", ""))
+
+for i in llist:
+    vw = Words(i)
+    wvl = vw.load()
+    dlist.append(wvl)
+ivect = WordVector(dlist)
+vect = ivect.load()
+
 def main():
     global user
+    global vect
+    global dlist
+    answ = ""
+    searchlist = []
+    instb = []
     user = input("\nYou: ")
     logging.info(user)
-    ai(user)
-def ai(data):
-    if not data:
+    if not user:
         main()
-    elif data == "exit":
+    elif user == "exit":
         exit()
-    elif len(data) > 40:
-        wrong()
-    send = App(data)
-    s = send.getdata()
-    if s == "":
+    word = Words(user)
+    w = word.load()
+
+    for i in dlist:
+        for j in i:
+            instb.append(j)
+    for k in w:
+        for i, x in enumerate(instb):
+            if x == k:
+                searchlist.append(i)
+
+    if len(searchlist) != 0:
+        for j in vect:
+            count = 0
+            for i in searchlist:
+                if j[i] != 0:
+                    count += 1
+                    if count == len(dlist[vect.index(j)]):
+                        answ = rlist[vect.index(j)]
+    if answ == "":
         wrong()
     else:
-        print("\nChatbot: {}".format(s))
-        logging.info(s)
+        print("\nChatbot: {}".format(answ))
+        logging.info(answ)
 
 def wrong():
     print("\nChatbot: {}".format(reply))
